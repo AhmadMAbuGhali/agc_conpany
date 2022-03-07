e18cfd8c10d6614df70e64963e1d8cb4ee942027
ahmad19678 <38799135+ahmad19678@users.noreply.github.com>
2022-03-07 19:07:40 +0200

    splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,43 @@
 import 'package:agc_conpany/test.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:splash_screen_view/SplashScreenView.dart';
+
 
 void main() {
   runApp( MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
 
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+
     return ScreenUtilInit(
         designSize: Size(375, 812),
         minTextAdapt: true,
         splitScreenMode: true,
         builder: () =>  MaterialApp(
-              home: Test(),
+          debugShowCheckedModeBanner: false,
+              home: SplashScreenView(
+                navigateRoute: Test(),
+                duration: 10000,
+                imageSize: 233,
+                imageSrc: 'assets/images/splahlogo.png',
+                backgroundColor: Colors.white,
+                text: "مجموعة أبناء أبوغالي للإستثمار",
+                textType: TextType.TyperAnimatedText,
+                textStyle: const TextStyle(
+                  fontSize: 30.0,
+                ),
+              ),
             ));
     // return MaterialApp(
     //   title: 'Flutter Demo',
