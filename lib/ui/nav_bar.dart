a4e1f0126b1819169eb452edc9ce76d9d9fa5a59
ahmad19678 <38799135+ahmad19678@users.noreply.github.com>
2022-03-07 21:53:26 +0200

    nav bar with text

--- lib/ui/nav_bar.dart
+++ lib/ui/nav_bar.dart
@@ -2,6 +2,8 @@ import 'package:agc_conpany/ui/registration/login.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 
+import '../themes/themes.dart';
+
 class NavScreen extends StatefulWidget {
   @override
   State<NavScreen> createState() => _HomeScreenState();
@@ -18,16 +20,41 @@ class _HomeScreenState extends State<NavScreen> {
     return Scaffold(
         bottomNavigationBar: CurvedNavigationBar(
           key: _bottomNavigationKey,
+
           index: 0,
-          height: 60.0,
-          items: const <Widget>[
-            Icon(Icons.movie, size: 30),
-            Icon(Icons.favorite, size: 30),
-            Icon(Icons.notifications, size: 30),
-            Icon(Icons.account_circle, size: 30),
+          height: 50.0,
+          items:  <Widget>[
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Icon(Icons.movie, size: 20),
+                Text('Hello'),
+              ],
+            ),  Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Icon(Icons.account_circle, size: 20),
+                Text('Hello'),
+              ],
+            ),  Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Icon(Icons.notifications, size: 20),
+                Text('Hello'),
+              ],
+            ),  Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Icon(Icons.favorite, size: 20),
+                Text('Hello'),
+              ],
+            ),
+
+
+
           ],
-          color: Colors.blueAccent,
-          buttonBackgroundColor: Colors.blueAccent,
+          color:Colors.black.withOpacity(0.4),
+          buttonBackgroundColor: orangeClr ,
           backgroundColor: Colors.white,
           animationCurve: Curves.easeInOut,
           animationDuration: const Duration(milliseconds: 600),
