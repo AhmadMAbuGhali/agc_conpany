c85685c89ca32490fb742d849200dc80c009b67a
ahmad19678 <38799135+ahmad19678@users.noreply.github.com>
2022-06-01 07:51:25 +0300

    add product With Getx

--- lib/ui/company_member/accountant/add_product.dart
+++ lib/ui/company_member/accountant/add_product.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:agc_conpany/componant/componant.dart';
 import 'package:agc_conpany/controller/a.dart';
 import 'package:agc_conpany/controller/add_product_controller.dart';
@@ -6,6 +8,7 @@ import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:provider/provider.dart';
 
 import '../../../resources/styles_manager.dart';
@@ -23,219 +26,217 @@ class AddProductScreen extends StatelessWidget {
     5: 'سائق',
   };
 
-  @override
-  Widget build(BuildContext context) {
-    final AddProductController controller = Get.put(AddProductController());
-    return SafeArea(
-      child: Scaffold(
-        body: SingleChildScrollView(
-          scrollDirection: Axis.vertical,
-          child: Consumer<AuthProvider>(builder: (context, provider, x) {
-            return Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 27.h),
-              child: Column(
-                children: [
-                  Text(
-                    'أضافة منتج',
-                    style: getBoldStyle(color: Colors.black),
-                  ),
-                  SizedBox(
-                    height: 20.h,
-                  ),
-                  DottedBorder(
-                    borderType: BorderType.RRect,
-                    radius: Radius.circular(17.r),
-                    dashPattern: const <double>[8, 4],
-                    color: ColorManager.black,
-                    strokeCap: StrokeCap.butt,
-                    strokeWidth: 1,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.all(Radius.circular(15.r)),
-                      child: InkWell(
-                        onTap: () {
-                          provider.pickNewImage();
-                        },
-                        child: Container(
-                          height: 195.h,
-                          width: MediaQuery.of(context).size.width,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(17.r),
-                            color: ColorManager.white,
-                            boxShadow: [
-                              BoxShadow(
-                                offset: const Offset(0, 3),
-                                blurRadius: 6,
-                                color: Colors.black.withOpacity(0.16),
+
+    @override
+    Widget build(BuildContext context) {
+      final AddProductController controller = Get.put(AddProductController());
+      return SafeArea(
+          child: Scaffold(
+            body: SingleChildScrollView(
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 27.h),
+                child: Column(
+                  children: [
+                    Text(
+                      'أضافة منتج',
+                      style: getBoldStyle(color: Colors.black),
+                    ),
+                    SizedBox(
+                      height: 20.h,
+                    ),
+                    DottedBorder(
+                        borderType: BorderType.RRect,
+                        radius: Radius.circular(17.r),
+                        dashPattern: const <double>[8, 4],
+                        color: ColorManager.black,
+                        strokeCap: StrokeCap.butt,
+                        strokeWidth: 1,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.all(Radius.circular(15.r)),
+                          child:InkWell(
+                            onTap: (){
+                              controller.getImage(ImageSource.gallery);
+                            },
+                            child: Container(
+                              height: 195.h,
+                              width: MediaQuery.of(context).size.width,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(17.r),
+                                color: ColorManager.white,
+                                boxShadow: [
+                                  BoxShadow(
+                                    offset: const Offset(0, 3),
+                                    blurRadius: 6,
+                                    color: Colors.black.withOpacity(0.16),
+                                  ),
+                                ],
                               ),
-                            ],
+                              child:Obx(
+                                      ()=> controller.selectedImagePath.value == ''?
+                                  Center(
+                                    child: Text('اضف صورة المنتج'),
+                                  )
+                                      : Image.file(File(controller.selectedImagePath.value),
+                                    fit: BoxFit.contain,)
+
+                              ),
+                            ) ,
                           ),
-                          child: provider.file == null
-                              ? Center(
-                                  child: Text('اضف صورة المنتج'),
-                                )
-                              : Image(
-                                  image: FileImage(provider.file!),
-                                  fit: BoxFit.fill,
-                                ),
-                        ),
-                      ),
+                        )
                     ),
-                  ),
-                  SizedBox(
-                    height: 25.h,
-                  ),
-                  WidgetTextFieldProudct(
-                      controller: nameController,
-                      hintText: 'اسم المنتج',
-                      inputType: TextInputType.text),
-                  SizedBox(
-                    height: 25.h,
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(15.r),
-                      color: ColorManager.white,
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.2),
-                          spreadRadius: 0,
-                          blurRadius: 6,
-                          offset: Offset(0, 2), // changes position of shadow
-                        ),
-                      ],
+                    SizedBox(
+                      height: 25.h,
                     ),
-                    width: double.infinity,
-                    height: 60.h,
-                    padding:
-                        EdgeInsets.symmetric(horizontal: 10.w, vertical: 3.h),
-                    child: DropdownButtonFormField<String>(
-                      // margin: EdgeInsets.only(top: 20.h, left: 40.w, right: 40.w),
-                      decoration: const InputDecoration(
-                        focusedBorder: InputBorder.none,
-                        enabledBorder: InputBorder.none,
-                        errorBorder: InputBorder.none,
-                        disabledBorder: InputBorder.none,
-                      ),
-                      value: dropdownValue,
-                      items: categoryList
-                          .map((int num, String value) {
-                            return MapEntry(
-                                num,
-                                DropdownMenuItem<String>(
-                                  value: value,
-                                  child: Text(value),
-                                ));
-                          })
-                          .values
-                          .toList(),
-                      onChanged: (vlaue) {
-                        dropdownValue = vlaue!;
-                      },
+                    WidgetTextFieldProudct(
+                        controller: nameController,
+                        hintText: 'اسم المنتج',
+                        inputType: TextInputType.text),
+                    SizedBox(
+                      height: 25.h,
                     ),
-                  ),
-                  SizedBox(
-                    height: 15.h,
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(27.r),
-                      color: ColorManager.white,
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.2),
-                          spreadRadius: 0,
-                          blurRadius: 6,
-                          offset: Offset(0, 2), // changes position of shadow
-                        ),
-                      ],
-                    ),
-                    width: double.infinity,
-                    child: Obx(
-                      () => Column(
-                        children: [
-                          Text("الاوزان المتاحة"),
-                          CheckboxListTile(
-                            title: Text("50 كيلو"),
-                            value: controller.checkBox50.value,
-                            onChanged: (value) {
-                              controller.checkBox50.value =
-                                  !controller.checkBox50.value;
-                            },
-                            controlAffinity: ListTileControlAffinity
-                                .leading, //  <-- leading Checkbox
-                          ),
-                          CheckboxListTile(
-                            title: Text("100 كيلو"),
-                            value: controller.checkBox100.value,
-                            onChanged: (value) {
-                              controller.checkBox100.value =
-                                  !controller.checkBox100.value;
-                            },
-                            controlAffinity: ListTileControlAffinity
-                                .leading, //  <-- leading Checkbox
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15.r),
+                        color: ColorManager.white,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.2),
+                            spreadRadius: 0,
+                            blurRadius: 6,
+                            offset: Offset(0, 2), // changes position of shadow
                           ),
                         ],
                       ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 10.h,
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(27.r),
-                      color: ColorManager.white,
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.2),
-                          spreadRadius: 0,
-                          blurRadius: 6,
-                          offset: Offset(0, 2), // changes position of shadow
+                      width: double.infinity,
+                      height: 60.h,
+                      padding:
+                      EdgeInsets.symmetric(horizontal: 10.w, vertical: 3.h),
+                      child: DropdownButtonFormField<String>(
+                        // margin: EdgeInsets.only(top: 20.h, left: 40.w, right: 40.w),
+                        decoration: const InputDecoration(
+                          focusedBorder: InputBorder.none,
+                          enabledBorder: InputBorder.none,
+                          errorBorder: InputBorder.none,
+                          disabledBorder: InputBorder.none,
                         ),
-                      ],
-                    ),
-                    padding:
-                        EdgeInsets.symmetric(horizontal: 10.w, vertical: 5.h),
-                    child: TextField(
-                      maxLines: 4,
-                      decoration:
-                          InputDecoration.collapsed(hintText: "وصف المنتج"),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 30.h,
-                  ),
-                  Container(
-                    width: double.infinity,
-                    height: 50.h,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(27.r),
-                      color: ColorManager.white,
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.2),
-                          spreadRadius: 0,
-                          blurRadius: 6,
-                          offset: Offset(0, 2), // changes position of shadow
-                        ),
-                      ],
-                    ),
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        primary: ColorManager.primary,
-                      elevation: 0.0,
+                        value: dropdownValue,
+                        items: categoryList
+                            .map((int num, String value) {
+                          return MapEntry(
+                              num,
+                              DropdownMenuItem<String>(
+                                value: value,
+                                child: Text(value),
+                              ));
+                        })
+                            .values
+                            .toList(),
+                        onChanged: (vlaue) {
+                          dropdownValue = vlaue!;
+                        },
                       ),
-
-                      onPressed: () {},
-                      child: Text('إضافة المنتج'),
                     ),
-                  )
-                ],
+                    SizedBox(
+                      height: 15.h,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(27.r),
+                        color: ColorManager.white,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.2),
+                            spreadRadius: 0,
+                            blurRadius: 6,
+                            offset: Offset(0, 2), // changes position of shadow
+                          ),
+                        ],
+                      ),
+                      width: double.infinity,
+                      child: Obx(
+                            () => Column(
+                          children: [
+                            Text("الاوزان المتاحة"),
+                            CheckboxListTile(
+                              title: Text("50 كيلو"),
+                              value: controller.checkBox50.value,
+                              onChanged: (value) {
+                                controller.checkBox50.value =
+                                !controller.checkBox50.value;
+                              },
+                              controlAffinity: ListTileControlAffinity
+                                  .leading, //  <-- leading Checkbox
+                            ),
+                            CheckboxListTile(
+                              title: Text("100 كيلو"),
+                              value: controller.checkBox100.value,
+                              onChanged: (value) {
+                                controller.checkBox100.value =
+                                !controller.checkBox100.value;
+                              },
+                              controlAffinity: ListTileControlAffinity
+                                  .leading, //  <-- leading Checkbox
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10.h,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(27.r),
+                        color: ColorManager.white,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.2),
+                            spreadRadius: 0,
+                            blurRadius: 6,
+                            offset: Offset(0, 2), // changes position of shadow
+                          ),
+                        ],
+                      ),
+                      padding:
+                      EdgeInsets.symmetric(horizontal: 10.w, vertical: 5.h),
+                      child: TextField(
+                        maxLines: 4,
+                        decoration:
+                        InputDecoration.collapsed(hintText: "وصف المنتج"),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30.h,
+                    ),
+                    Container(
+                      width: double.infinity,
+                      height: 50.h,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(27.r),
+                        color: ColorManager.white,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.2),
+                            spreadRadius: 0,
+                            blurRadius: 6,
+                            offset: Offset(0, 2), // changes position of shadow
+                          ),
+                        ],
+                      ),
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: ColorManager.primary,
+                          elevation: 0.0,
+                        ),
+
+                        onPressed: () {},
+                        child: Text('إضافة المنتج'),
+                      ),
+                    )
+                  ],
+                ),
               ),
-            );
-          }),
-        ),
-      ),
-    );
+            ),
+          ));
+    }
   }
-}
