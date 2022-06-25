18b704f75d007521bd1c838afbb4f7250e7c281e
mac <oma98kam@gmail.com>
2022-06-26 02:28:40 +0300

    completed order

--- lib/ui/company_member/driver/driver_delevery.dart
+++ lib/ui/company_member/driver/driver_delevery.dart
@@ -30,66 +30,68 @@ TextEditingController numberOfOrder=TextEditingController();
                   style: getBoldStyle(color: ColorManager.primary,fontSize: FontSize.s22),
                 ),
 
-                ListView.builder(
-                  itemBuilder:(context, index) {
-                    return DriverToCustomer(
-                      order:   provider.orderDriverpinding[index]
-                    ,
-                        onPress:()=> showDialog(
-                      context: context,
-                      builder: (ctx) =>  AlertDialog(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30.r),
-                        ),
-                        title: Center(child: Text('أدخل رقم الطلبية',style: getBoldStyle(color: ColorManager.textOrange,fontSize: FontSize.s24),)),
-                        content:   SizedBox(
-                          height: 50,
-                          child: TextField(
-                            controller: numberOfOrder,
-                            decoration: const InputDecoration(
-                              border: OutlineInputBorder(),
+                Expanded(
+                  child: ListView.builder(
+                    itemBuilder:(context, index) {
+                      return DriverToCustomer(
+                        order:   provider.orderDriverpinding[index]
+                      ,
+                          onPress:()=> showDialog(
+                        context: context,
+                        builder: (ctx) =>  AlertDialog(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30.r),
+                          ),
+                          title: Center(child: Text('أدخل رقم الطلبية',style: getBoldStyle(color: ColorManager.textOrange,fontSize: FontSize.s24),)),
+                          content:   SizedBox(
+                            height: 50,
+                            child: TextField(
+                              controller: numberOfOrder,
+                              decoration: const InputDecoration(
+                                border: OutlineInputBorder(),
+                              ),
                             ),
                           ),
-                        ),
-                        actions: <Widget>[
-                          Container(
-                            margin: EdgeInsets.only(bottom: 15.h),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: [
-                                SizedBox(
-                                  width: 100.w,
-                                  height: 30.h,
-                                  child: ElevatedButton(onPressed: (){
-                                    provider.orderDriverpinding[index].orderNumber=numberOfOrder.text;
-                                    provider.orderDriverpinding[index].status='Completed';
-                                  provider.addToCompleteOrder(provider.orderDriverpinding[index]);
-                                  provider.deleteFromOrderDriverpinding(provider.orderDriverpinding[index].id!);
-                                  }, child: Text('ارسال',style: getRegularStyle(color: ColorManager.white,fontSize: FontSize.s13),),style: ElevatedButton.styleFrom(
-                                      primary: ColorManager.primary,
-                                      elevation: 1,
-                                      shape: RoundedRectangleBorder(
-                                          borderRadius: BorderRadius.circular(AppSize.s5))
-                                  ),),
-                                ),   SizedBox(
-                                  width: 100.w,
-                                  height: 30.h,
-                                  child: ElevatedButton(onPressed: (){
-                                    Get.back();
-                                  }, child: Text('الغاءا',style: getRegularStyle(color: ColorManager.black,fontSize: FontSize.s13),),style: ElevatedButton.styleFrom(
-                                      primary: ColorManager.white,
-                                      side: BorderSide(width: 1.0, color: ColorManager.black,),elevation: 1, shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(AppSize.s5))
-                                  ),),
-                                ),
-                              ],
+                          actions: <Widget>[
+                            Container(
+                              margin: EdgeInsets.only(bottom: 15.h),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  SizedBox(
+                                    width: 100.w,
+                                    height: 30.h,
+                                    child: ElevatedButton(onPressed: (){
+                                      provider.orderDriverpinding[index].orderNumber=numberOfOrder.text;
+                                      provider.orderDriverpinding[index].status='Completed';
+                                    provider.addToCompleteOrder(provider.orderDriverpinding[index]);
+                                    provider.deleteFromOrderDriverpinding(provider.orderDriverpinding[index].id!);
+                                    }, child: Text('ارسال',style: getRegularStyle(color: ColorManager.white,fontSize: FontSize.s13),),style: ElevatedButton.styleFrom(
+                                        primary: ColorManager.primary,
+                                        elevation: 1,
+                                        shape: RoundedRectangleBorder(
+                                            borderRadius: BorderRadius.circular(AppSize.s5))
+                                    ),),
+                                  ),   SizedBox(
+                                    width: 100.w,
+                                    height: 30.h,
+                                    child: ElevatedButton(onPressed: (){
+                                      Get.back();
+                                    }, child: Text('الغاءا',style: getRegularStyle(color: ColorManager.black,fontSize: FontSize.s13),),style: ElevatedButton.styleFrom(
+                                        primary: ColorManager.white,
+                                        side: BorderSide(width: 1.0, color: ColorManager.black,),elevation: 1, shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(AppSize.s5))
+                                    ),),
+                                  ),
+                                ],
+                              ),
                             ),
-                          ),
 
-                        ],
-                      ),
-                    ));
-                  } ,itemCount:provider.orderDriverpinding.length,),
+                          ],
+                        ),
+                      ));
+                    } ,itemCount:provider.orderDriverpinding.length,),
+                ),
 
               ],
             ),
